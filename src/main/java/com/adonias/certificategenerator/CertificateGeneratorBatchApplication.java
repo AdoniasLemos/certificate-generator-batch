a1b1f845509cbebf80c1da0f1bff35f68f7961e9
Adonias Silva <adoniaslg@gmail.com>
2021-11-11 17:07:23 -0300

    Initial commit

---
+++ src/main/java/com/adonias/certificategenerator/CertificateGeneratorBatchApplication.java
@@ -0,0 +1,13 @@
+package com.adonias.certificategenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateGeneratorBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateGeneratorBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adonias/certificategenerator/CertificateGeneratorBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adonias.certificategenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateGeneratorBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
